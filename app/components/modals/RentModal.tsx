38efae97c2235027b93d1b261d8be222f4b2e93a
YutaNagai0624 <skmsskss@gmail.com>
2023-05-17 15:19:23 +0800

    counter input component

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,6 +5,8 @@ import Modal from "./Modal"
 import CategoryInput from "../inputs/CategoryInput";
 import { Heading } from "../Heading";
 import CountrySelect from "../inputs/CountrySelect";
+import Counter from "../inputs/Counter";
+
 
 import { useMemo, useState } from "react";
 import { categories } from "../navbar/Categories";
@@ -50,6 +52,9 @@ const RentModal = () => {
 
     const location = watch('location');
     const category = watch('category');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
     const Map = useMemo(() => dynamic(() => import('../Map'), { 
         ssr: false 
@@ -124,6 +129,38 @@ const RentModal = () => {
           </div>
         );
       }
+
+    if (step === STEPS.INFO){
+        bodyContent = (
+          <div className="flex flex-col gap-8">
+            <Heading
+              title="Share some basics about your place"
+              subtitle="What amenities you have?"
+            />
+            <Counter
+              title="Guests"
+              subtitle="How many guests do you allow?"
+              value={guestCount}
+              onChange={(value) => setCustomValue('guestCount', value)}
+            />
+            <hr />
+            <Counter
+              title="Rooms"
+              subtitle="How many rooms do you have?"
+              value={roomCount}
+              onChange={(value) => setCustomValue('roomCount', value)}
+            />
+            <hr />
+            <Counter
+              title="Bathrooms"
+              subtitle="How many bathrooms do you have?"
+              value={bathroomCount}
+              onChange={(value) => setCustomValue('bathroomCount', value)}
+            />
+
+          </div>
+        )
+    }
 
     return (
         <Modal
