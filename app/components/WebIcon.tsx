5f1fcb150c4adfee6370eb70c0b71185eee596fa
Haga-kure <dd4184968@gmail.com>
2023-05-22 16:42:02 +0800

    WebIcon

---
+++ app/components/WebIcon.tsx
@@ -0,0 +1,18 @@
+"use clint";
+
+const WebIcon = () => {
+  return (
+    <>
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/WebIcon.ico"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+    </>
+  );
+};
+export default WebIcon;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
-import { Nunito } from 'next/font/google'
-import Navbar from './components/navbar/Navbar';
-import ClientOnly from './components/ClientOnly';
-import './globals.css' 
-import RegisterModal  from './components/modals/RegisterModal';
-import ToasterProvider from './providers/ToasterProvider';
-import LoginModal from './components/modals/LoginModal';
-import getCurrentUser from './actions/getCurrentUser';
-import RentModal from './components/modals/RentModal';
-import SearchModal from './components/modals/SearchModal';
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import "./globals.css";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
+import WebIcon from "./components/WebIcon";
 
 export const metadata = {
-  title: 'Merit Project',
-  description: 'Merit Project',
-}
+  title: "Merit Project",
+  description: "Merit Project",
+};
 
 const font = Nunito({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const currentUser = await getCurrentUser()
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
+      <WebIcon />
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
@@ -35,10 +37,8 @@ export default async function RootLayout({
           <RegisterModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className='pb-20 pt-28'>
-          {children}
-        </div>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
-  )
+  );
 }
