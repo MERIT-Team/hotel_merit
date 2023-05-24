b8d04c50bc4c90080c8e244c486dbd7eadf9deb1
Sed-Erdene <zr.freeze05@gmail.com>
2023-05-24 16:14:11 +0800

    fixed register error

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,25 +3,23 @@ import bcrypt from "bcrypt";
 
 import prisma from "@/app/libs/prismadb";
 
-export async function POST(
-  request: Request, 
-) {
+export async function POST(request: Request) {
   const body = await request.json();
-  const { 
-    email,
-    name,
-    password,
-   } = body;
+  const { email, name, password } = body;
 
-   const hashedPassword = await bcrypt.hash(password, 12);
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-   const user = await prisma.user.create({
-    data: {
-      email,
-      name,
-      hashedPassword,
-    }
-  });
-
-  return NextResponse.json(user);
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+    return NextResponse.json(user);
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json("Email duplicated", { status: 400 });
+  }
 }
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
@@ -11,124 +11,122 @@ import { signIn } from "next-auth/react";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
 
-import  Modal  from "./Modal";
+import Modal from "./Modal";
 import { Heading } from "../Heading";
-import Button  from "../Button";
+import Button from "../Button";
 import Input from "../inputs/Input";
 
-
-
 const RegisterModal = () => {
-    const registerModal = useRegisterModal();
-    const loginModal = useLoginModal();
-    const [isLoading, setIsLoading] = useState(false);
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const { 
-      register, 
-      handleSubmit,
-      formState: {
-        errors,
-      },
-    } = useForm<FieldValues>({
-      defaultValues: {
-        name: '',
-        email: '',
-        password: ''
-      },
-    });
-  
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        setIsLoading(true); 
-        
-        axios.post('/api/register', data)
-        .then(() =>{
-            toast.success('Registered!');
-            registerModal.onClose();
-            loginModal.onOpen();
-        })
-        .catch((error) => {
-            toast.error(error); 
-        })
-        .finally(() => {
-            setIsLoading(false)
-        })
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-    const onToggle = useCallback(() => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        toast.success("Registered!");
         registerModal.onClose();
         loginModal.onOpen();
-      }, [registerModal, loginModal])
+      })
+      .catch((error) => {
+        toast.error(error.response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-    const bodyContent = (
-        <div className="flex flex-col gap-4">
-            <Heading title="Welcome to MERIT" subtitle="Create an account!"/>
-            <Input
-                id="email"
-                label="Email"
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-            />
-            <Input
-                id="name"
-                label="Name"
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-            />
-            <Input
-                id="password"
-                label="Password"
-                type="password"
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-            />
-        </div>
-    )
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [registerModal, loginModal]);
 
-    const footerContent = (
-        <div className="flex flex-col gap-4 mt-3">
-            <hr/>
-            <Button 
-              outline 
-              label="Continue with Google"
-              icon={FcGoogle}
-              onClick={() => signIn('google')} 
-            />
-            <Button 
-              outline 
-              label="Continue with Github"
-              icon={AiFillGithub}
-              onClick={() => signIn('github')}
-            />
-            <div className="text-neutral-500 text-center mt-4 font-light">
-                <div className="justify-center flex flex-row items-center gap-2">
-                    <div>
-                        Already have an account? 
-                    </div>
-                    <div className="text-neutral-800 cursor-pointer hover:underline" onClick={onToggle}>
-                        Log in
-                    </div> 
-                </div>
-            </div>
-        </div>
-    )
-
-    return(
-        <Modal
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to MERIT" subtitle="Create an account!" />
+      <Input
+        id="email"
+        label="Email"
         disabled={isLoading}
-        isOpen={registerModal.isOpen}
-        title="Register"
-        actionLabel="Continue"
-        onClose={registerModal.onClose}
-        onSubmit={handleSubmit(onSubmit)}
-        body={bodyContent}
-        footer={footerContent}
+        register={register}
+        errors={errors}
+        required
       />
-    )
-}
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => signIn("google")}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn("github")}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            className="text-neutral-800 cursor-pointer hover:underline"
+            onClick={onToggle}
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
 export default RegisterModal;
